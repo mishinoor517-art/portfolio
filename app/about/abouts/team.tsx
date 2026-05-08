83547d68860af2347e403e33750d7cdec9e46482
Mishinoor <mishinoor517-art@github.com>
2026-05-08 13:56:43 +0100

    updated project

---
+++ app/about/abouts/team.tsx
@@ -0,0 +1,13 @@
+
+"use client"
+
+
+import React from 'react'
+
+function team() {
+  return (
+    <div>team</div>
+  )
+}
+
+export default team
---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import Team from './abouts/team'
+function teamPage() {
+  return (
+    <div>
+      <h1>Team Page</h1>
+      <Team />
+        </div>
+  )
+}
+
+export default teamPage
